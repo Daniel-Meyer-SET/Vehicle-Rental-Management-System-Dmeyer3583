1d50c76e90c71c63d0733a60c23c33d9750b7093
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-03-17 21:39:43 -0400

    added logic to create truck and motorcycle

--- Program.cs
+++ Program.cs
@@ -6,20 +6,19 @@ namespace Vehicle_Rental_Management_System
     {   // main method and entry point
         static void Main(string[] args)
         {
-         Console.WriteLine("Welcome to the Vehicle Rental Management System");
+            Console.WriteLine("Welcome to the Vehicle Rental Management System");
             Console.WriteLine("Options:\n A: Add new vehicle\n R:Rent a vehicle \nX: Remove A vehicle\nS:Show Details");
-            
+
             RentalAgency rental = new RentalAgency();
-            switch (Console.ReadKey(false).KeyChar) { 
-            case 'a':
-                    {
-                        Console.WriteLine("c: car\nt:truck\nm:motorcycle");
-                        switch (Console.ReadKey(false).KeyChar)
-                        {
+            switch (Console.ReadKey(false).KeyChar)
+            {
+                case 'a':{
+                                Console.WriteLine("c: car\nt:truck\nm:motorcycle");
+                                 switch (Console.ReadKey(false).KeyChar) {
                             case 'c':
                                 {
-                                Console.WriteLine("Car:");
-                                   Car car = new Car();
+                                    Console.WriteLine("Car:");
+                                    Car car = new Car();
                                     Console.WriteLine("enter year:");
                                     car.Year = int.Parse(Console.ReadLine());
                                     Console.WriteLine("enter manufacturer");
@@ -29,17 +28,20 @@ namespace Vehicle_Rental_Management_System
                                     Console.WriteLine("enter number of seats:");
                                     car.Seats = int.Parse(Console.ReadLine());
                                     Console.WriteLine("Automatic Or Manual Transmission:");
-                                    switch (Console.ReadKey(false).KeyChar) { 
-                                    case 'a': {
+                                    switch (Console.ReadKey(false).KeyChar)
+                                    {
+                                        case 'a':
+                                            {
 
-                                    car.Transmission = "Automatic";
-                                    break;
-                                      }
-                                    case 'm': {
-                                     car.Transmission = "Manual";
-                                    break;        
-                                    }
-                                   
+                                                car.Transmission = "Automatic";
+                                                break;
+                                            }
+                                        case 'm':
+                                            {
+                                                car.Transmission = "Manual";
+                                                break;
+                                            }
+
                                     }
                                     Console.WriteLine("is the car a convertible?");
                                     switch (Console.ReadKey(false).KeyChar)
@@ -56,24 +58,92 @@ namespace Vehicle_Rental_Management_System
                                                 break;
                                             }
 
+
                                     }
 
 
 
                                     break;
                                 }
+                            case 't':
+                                {
+                                    Console.WriteLine("Truck:");
+                                    Truck truck = new Truck();
+                                    Console.WriteLine("enter year:");
+                                    truck.Year = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("enter manufacturer");
+                                    truck.Manufacturer = Console.ReadLine();
+                                    Console.WriteLine("Enter model");
+                                    truck.Model = Console.ReadLine();
+                                    Console.WriteLine("enter capacity:");
+                                    truck.Capacity = int.Parse(Console.ReadLine());
+
+                                    Console.WriteLine("is the truck four wheel drive?");
+                                    Console.WriteLine("what type of truck:");
+                                    truck.TruckType = Console.ReadLine();
+                                    switch (Console.ReadKey(false).KeyChar)
+                                    {
+                                        case 'y':
+                                            {
+
+                                                truck.FourWheelDrive = true;
+                                                break;
+                                            }
+                                        case 'n':
+                                            {
+                                                truck.FourWheelDrive = false;
+                                                break;
+                                            }
+                                    }
+                                    break;
+                                }
+                            case 'm':
+                                {
+                                    Console.WriteLine("Motorcycle:");
+                                    Motorcycle motorcycle = new Motorcycle();
+                                    Console.WriteLine("enter year:");
+                                    motorcycle.Year = int.Parse(Console.ReadLine());
+                                    Console.WriteLine("enter manufacturer");
+                                    motorcycle.Manufacturer = Console.ReadLine();
+                                    Console.WriteLine("Enter model");
+                                    motorcycle.Model = Console.ReadLine();
+                                    Console.WriteLine("Enter fuel type");
+                                    motorcycle.FuelType = Console.ReadLine();
+                                    Console.Write("Does The Motorcyle have a fairing?");
+                                    switch (Console.ReadKey(false).KeyChar)
+                                    {
+                                        case 'y':
+                                            {
+
+                                                motorcycle.HasFairing = true;
+                                                break;
+                                            }
+                                        case 'n':
+                                            {
+                                                motorcycle.HasFairing = false;
+                                                break;
+                                            }
+
+                                    }
+                                    break;
+                                
+                                }
+
                         }
                         break;
                     }
-            
+                case 'r': {
+                      Console.Write
+                        
+                        break;
+                    }
+
+                    
+
             }
-
-
-
-
         }
-
-        static void showVehiclesInFleet(RentalAgency rental) {
+        static void showVehiclesInFleet(RentalAgency rental)
+        {
 
             foreach (Vehicle vehicle1 in rental.Fleet)
             {
@@ -81,7 +151,6 @@ namespace Vehicle_Rental_Management_System
 
             }
         }
-
     }
 }
 
